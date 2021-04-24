495d1377b9542f170d5d16dd3dd7742466817a53
ajdafonte <4990971+ajdafonte@users.noreply.github.com>
2021-04-25 00:19:57 +0200

    Change package name

--- src/test/java/pt/caires/lottery/LotteryServiceApplicationTests.java
+++ src/test/java/pt/caires/lottery/LotteryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package pt.caires.lotteryservice;
+package pt.caires.lottery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LotteryServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
