781f3ee66d98f4bf2ab02b71c590b40a8ad03b68
ajdafonte <ajdafonte@gmail.com>
2021-04-24 23:55:27 +0200

    setup project

---
+++ src/test/java/pt/caires/lotteryservice/LotteryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.caires.lotteryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
